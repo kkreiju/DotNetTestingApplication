2bcac81eb62bcb40fa61db0ffda2b7f36997f23f
kkreiju <138178348+kkreiju@users.noreply.github.com>
2024-08-22 02:34:51 +0800

    crud done

--- test/Controllers/StudentsController.cs
+++ test/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using test.Data;
 using test.Models;
 using test.Models.Entities;
@@ -36,5 +37,54 @@ namespace test.Controllers
             await dbContext.SaveChangesAsync();
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+
+            return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(StudentIdentity viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.StudID);
+
+            if(student is not null)
+            {
+                student.StudName = viewModel.StudName;
+                student.StudEmail = viewModel.StudEmail;
+                student.StudPhone = viewModel.StudPhone;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(StudentIdentity viewModel)
+        {
+            var student = await dbContext.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.StudID == viewModel.StudID);
+
+            if(student is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+
+			return RedirectToAction("List", "Students");
+		}
     }
 }
